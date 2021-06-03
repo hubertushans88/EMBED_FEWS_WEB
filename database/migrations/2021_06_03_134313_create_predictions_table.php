0919dd13cc2a3f94e886318e10d20b986eac6147
Hubertus Hans <hubertushans18@gmail.com>
2021-06-03 21:02:59 +0700

    initial commit

---
+++ database/migrations/2021_06_03_134313_create_predictions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePredictionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('predictions', function (Blueprint $table) {
+            $table->id();
+            $table->string('node');
+            $table->double('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('predictions');
+    }
+}
